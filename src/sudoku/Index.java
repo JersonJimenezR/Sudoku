dedd1691d86ce4ffd99766b4ef0d66b5c4ba1648
JersonJimenez <jerson.ti@onoff.com.co>
2018-08-20 08:34:40 -0500

    Agregando proceso de busqueda

--- src/sudoku/Index.java
+++ src/sudoku/Index.java
@@ -30,19 +30,20 @@ public class Index {
         this.options(sc.nextInt());             
     }
     
+    
     //--Opción seleccionada por el usuario
     
     public void options(int option)
     {
         switch (option) 
         {
-            case 1:
+            case 1: //--Sudoku Fácil
                 
                 System.out.println("Seleccionaste sudoku Fácil \n");
                 this.printSudoku(easy.initEasy());
                 System.out.println();
                 System.out.println("¿Resolver S/N ?");
-                this.decision(sc.next());
+                this.decision(sc.next() , easy.initEasy());
                 break;
                 
                 
@@ -70,7 +71,7 @@ public class Index {
     }
     
     
-    public void decision(String s)
+    public void decision(String s , String[][] sudoku)
     {
         if("N".equals(s))
         {            
@@ -80,9 +81,184 @@ public class Index {
         {
             //--Resolver soduko
             
-            //this.solve();
+            System.out.println(this.solve(sudoku));
         }
     }
     
+    
+    public String solve(String[][] sudoku)
+    {
+        //-- Busqueda de matriz 3x3 con menos espacios vacíos 
+        
+        int[] XY = this.busqueda(sudoku);
+        String[][] M3x3 = new String[3][3];
+        M3x3 = this.matriz3x3(sudoku, XY[1] , XY[2]);
+        return this.poner(sudoku , M3x3 , XY);
+
+    }
+    
+    public int[] busqueda(String[][] sudoku)
+    {        
+        int numeros = 0;        
+        int C = 3; //-- Posición Columna
+        int F = 3; //-- Posición Fila
+        int[] M = new int[3]; //--Matriz que lleva el mayor número de números
+        int K = 0; //--Última columna de M
+        
+        for (int i = 0; i < 9; i++) 
+        {     
+            if(i%F != 0)
+            {
+                for (int j = C-3; j >= 0; j++) 
+                {
+                    if(j%C != 0)
+                    {  
+                        if(sudoku[i][j] != " ")
+                        {
+                            numeros++;
+                        }
+
+                    }else
+                    {
+                        K = j-1;
+                        break;
+                    }                      
+                }
+                
+            }else
+            {
+                //-- Si la cantidad de números de la matriz 3x3 es mayor a la anterior entonces esta será la nueva matriz de busqueda
+                
+                if(numeros > M[0])
+                {
+                    M[0] = numeros;
+                    M[1] = i;
+                    M[2] = K;
+                }
+                
+                C +=3;
+                
+                if(C > 9)
+                {
+                    C = 0;
+                    i += 3;
+                }                
+            }            
+        }
+                
+        return M;
+    }
+    
+    
+    public String[][] matriz3x3(String[][] sudoku , int a , int b)
+    {
+        String[][] sudoku3x3 = new String[3][3];
+        
+        for (int i = a-2; i <= a; i++) 
+        {
+            for (int j = b-2; j <= b; j++) 
+            {
+                sudoku3x3[i][j] = sudoku[i][j];
+            }
+        }
+        
+        return sudoku3x3;
+    }
+    
+    
+    public String poner(String[][] sudoku , String[][] M3x3 , int[] XY)
+    {
+        String poner = this.evaluarNumero(sudoku, M3x3, XY);
+        
+        return poner;
+    }
+    
+    
+    public String evaluarNumero(String[][] sudoku , String[][] M3x3 , int[] XY)
+    {
+        int numero = 1; //--Número a evaluar en el sudoku
+        int opciones = 0; //--Número de posibilidades que tiene un número evaluado
+        int[] vetados = new int[9]; //--Vector de números que no pueden estar en la matriz 3x3
+        String poner = " "; //--Número a poner en la matriz        
+        boolean V = false; //--Control de condicional vetados
+        
+        int i = 0; 
+        while(i < 3)
+        {       
+            for (int j = 0; j < 3; j++) 
+            {                
+                if(" ".equals(M3x3[i][j]))
+                {
+                    if(this.evaluarNumero3x3(M3x3, Integer.toString(numero)))
+                    {
+                        if(this.evaluarNumeroFC(sudoku , XY[0] - 2-i , XY[1] - 2-j , Integer.toString(numero)))
+                        {
+                            poner = Integer.toString(numero);                        
+                            opciones++;
+
+                            if(opciones > 1)
+                            {
+                                vetados[numero-1] = numero;
+                                numero++;
+                                poner = " ";
+                                opciones = 0;
+                                V = true;                                                        
+                            }
+                        }                        
+                    }                    
+                }
+            }
+            
+            if(V)
+            {
+                i=0;
+                V = false;
+            }else
+            {
+                i++;
+            }            
+        }
+        
+        return poner;
+    }
+    
+    
+    public boolean evaluarNumero3x3(String[][] M3x3 , String numero)
+    {
+        boolean respuesta = false;
+        
+        for (int i = 0; i < 3; i++) 
+        {
+            for (int j = 0; j < 3; j++) 
+            {
+                if(M3x3[i][j] != numero)
+                {
+                    respuesta = true;
+                }
+            }
+        }
+        
+        return respuesta;
+    }
+    
+        
+    public boolean evaluarNumeroFC(String[][] sudoku , int a , int b , String numero)
+    {
+        boolean respuesta = false;
+        
+        for (int i = 0; i < 9; i++) 
+        {
+            if(sudoku[a][i] != numero)
+                respuesta = true;
+        } 
+        
+        for (int i = 0; i < 3; i++) 
+        {
+            if(sudoku[i][b] == numero)
+                respuesta = false;
+        }
+        
+        return respuesta;
+    }
 
 }
--- src/sudoku/SudokuEasy.java
+++ src/sudoku/SudokuEasy.java
@@ -19,93 +19,111 @@ public class SudokuEasy {
     {
         String[][] sudokuEasy = new String[9][9];
         
+        //--A
+        
         sudokuEasy[0][0] = " ";
         sudokuEasy[0][1] = " ";
         sudokuEasy[0][2] = "3";        
+        sudokuEasy[1][0] = " ";
+        sudokuEasy[1][1] = "4";
+        sudokuEasy[1][2] = " ";   
+        sudokuEasy[2][0] = " ";
+        sudokuEasy[2][1] = "7";
+        sudokuEasy[2][2] = " ";  
+        
+        //--B
+        
         sudokuEasy[0][3] = "8";
         sudokuEasy[0][4] = " ";
         sudokuEasy[0][5] = " ";
-        sudokuEasy[0][6] = " ";
-        sudokuEasy[0][7] = "9";
-        sudokuEasy[0][8] = " ";
-        
-        sudokuEasy[1][0] = " ";
-        sudokuEasy[1][1] = "4";
-        sudokuEasy[1][2] = " ";        
         sudokuEasy[1][3] = "5";
         sudokuEasy[1][4] = "6";
         sudokuEasy[1][5] = "1";
-        sudokuEasy[1][6] = "3";
-        sudokuEasy[1][7] = "7";
-        sudokuEasy[1][8] = " ";
-       
-        sudokuEasy[2][0] = " ";
-        sudokuEasy[2][1] = "7";
-        sudokuEasy[2][2] = " ";        
         sudokuEasy[2][3] = " ";
         sudokuEasy[2][4] = "2";
         sudokuEasy[2][5] = " ";
+        
+        //--C
+        
+        sudokuEasy[0][6] = " ";
+        sudokuEasy[0][7] = "9";
+        sudokuEasy[0][8] = " ";
+        sudokuEasy[1][6] = "3";
+        sudokuEasy[1][7] = "7";
+        sudokuEasy[1][8] = " ";
         sudokuEasy[2][6] = " ";
         sudokuEasy[2][7] = " ";
         sudokuEasy[2][8] = " ";
         
+        
+        //--D
+        
         sudokuEasy[3][0] = " ";
         sudokuEasy[3][1] = "6";
         sudokuEasy[3][2] = " ";        
+        sudokuEasy[4][0] = "9";
+        sudokuEasy[4][1] = " ";
+        sudokuEasy[4][2] = " "; 
+        sudokuEasy[5][0] = " ";
+        sudokuEasy[5][1] = "1";
+        sudokuEasy[5][2] = "2";  
+        
+        //--E
+        
         sudokuEasy[3][3] = " ";
         sudokuEasy[3][4] = "5";
         sudokuEasy[3][5] = "4";
-        sudokuEasy[3][6] = " ";
-        sudokuEasy[3][7] = " ";
-        sudokuEasy[3][8] = "9";
-        
-                
-        sudokuEasy[4][0] = "9";
-        sudokuEasy[4][1] = " ";
-        sudokuEasy[4][2] = " ";        
         sudokuEasy[4][3] = "2";
         sudokuEasy[4][4] = " ";
         sudokuEasy[4][5] = " ";
-        sudokuEasy[4][6] = " ";
-        sudokuEasy[4][7] = " ";
-        sudokuEasy[4][8] = "6";
-        
-        sudokuEasy[5][0] = " ";
-        sudokuEasy[5][1] = "1";
-        sudokuEasy[5][2] = "2";        
         sudokuEasy[5][3] = " ";
         sudokuEasy[5][4] = " ";
         sudokuEasy[5][5] = " ";
+        
+        //--F
+        
+        sudokuEasy[3][6] = " ";
+        sudokuEasy[3][7] = " ";        
+        sudokuEasy[3][8] = " ";                
+        sudokuEasy[4][6] = " ";
+        sudokuEasy[4][7] = " ";
+        sudokuEasy[4][8] = "6";
         sudokuEasy[5][6] = " ";
         sudokuEasy[5][7] = "5";
         sudokuEasy[5][8] = "8";
         
+        //--G
+        
         sudokuEasy[6][0] = "1";
         sudokuEasy[6][1] = "2";
         sudokuEasy[6][2] = " ";        
+        sudokuEasy[7][0] = "3";
+        sudokuEasy[7][1] = "9";
+        sudokuEasy[7][2] = "6";  
+        sudokuEasy[8][0] = " ";
+        sudokuEasy[8][1] = " ";
+        sudokuEasy[8][2] = " ";
+        
+        //--H
+        
         sudokuEasy[6][3] = "3";
         sudokuEasy[6][4] = "9";
         sudokuEasy[6][5] = " ";
-        sudokuEasy[6][6] = "8";
-        sudokuEasy[6][7] = " ";
-        sudokuEasy[6][8] = "7";
-        
-        sudokuEasy[7][0] = "3";
-        sudokuEasy[7][1] = "9";
-        sudokuEasy[7][2] = "6";        
         sudokuEasy[7][3] = "7";
         sudokuEasy[7][4] = " ";
         sudokuEasy[7][5] = " ";
-        sudokuEasy[7][6] = "5";
-        sudokuEasy[7][7] = " ";
-        sudokuEasy[7][8] = " ";
-        
-        sudokuEasy[8][0] = " ";
-        sudokuEasy[8][1] = " ";
-        sudokuEasy[8][2] = " ";        
         sudokuEasy[8][3] = " ";
         sudokuEasy[8][4] = "1";
         sudokuEasy[8][5] = "6";
+        
+        //--I
+        
+        sudokuEasy[6][6] = "8";
+        sudokuEasy[6][7] = " ";
+        sudokuEasy[6][8] = "7";
+        sudokuEasy[7][6] = "5";
+        sudokuEasy[7][7] = " ";
+        sudokuEasy[7][8] = " ";
         sudokuEasy[8][6] = " ";
         sudokuEasy[8][7] = " ";
         sudokuEasy[8][8] = " ";
